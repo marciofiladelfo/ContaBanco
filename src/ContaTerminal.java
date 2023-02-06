7db6b82f3b8378d7b67932ba19707a5d046f1d2a
marciofiladelfo <marcio.kabana@gmail.com>
2023-02-06 17:13:11 -0300

    Implementado funcionalidades do projeto

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,2 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da Conta : ");
+        int conta = sc.nextInt();
+
+        System.out.print("Por favor, digite o número da Agência : ");
+        String agencia = sc.next();
+
+        System.out.print("Por favor, digite o nome do cliente : ");
+        String nome = sc.next();
+
+        System.out.print("Por favor, digite o saldo : ");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ",conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
+    }
 }
